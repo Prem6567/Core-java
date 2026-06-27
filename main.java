419bd599232f9637f5fb3fdff79fbc88b6cc0116
Prem6567 <premkumarkanojiya758@gmail.com>
2026-06-27 19:22:44 +0530

    First' program uploaded
    Initial upload of Java program using jvdriod ( main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+	int age = 18;
+	double marks = 95.5;
+	char grade = 'A';
+	boolean pass = true;
+	String name = "prem";
+	  
+	   System.out.println(age);
+	   System.out.println(marks);
+	   System.out.println(grade);
+	   System.out.println(pass);
+	   System.out.println(name);
+	}
+}
