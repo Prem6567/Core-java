d8659907f1b426f72556824f9caca58038f76ab7
Prem6567 <premkumarkanojiya758@gmail.com>
2026-06-29 10:46:09 +0530

    Update Hello World java program

--- main.java
+++ main.java
@@ -1,15 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-	int age = 18;
-	double marks = 95.5;
-	char grade = 'A';
-	boolean pass = true;
-	String name = "prem";
-	  
-	   System.out.println(age);
-	   System.out.println(marks);
-	   System.out.println(grade);
-	   System.out.println(pass);
-	   System.out.println(name);
+	 System.out.println("Hello,World!");
+	 System.out.print("Welcome to java");
 	}
 }
