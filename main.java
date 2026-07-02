518d2346d662cee7e9dc8f2ccbae1d8ef8b53afb
Prem6567 <premkumarkanojiya758@gmail.com>
2026-07-02 10:37:18 +0530

    Update Java basics output
     Java output program

--- main.java
+++ main.java
@@ -1,12 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 5;
-		System.out.println( a + b); //
-		System.out.println( a  -  b); //
-		System.out.println( a  * b); //
-		System.out.println( a / b ); //
-		System.out.println( a % b); //
+	    System.out.println("Hello");
+	    System.out.println("My Name is prem");
+	    System.out.println("I Am Learning Java");
 	}
 }
-		
