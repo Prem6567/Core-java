4b8be16a19ee1e4f2ef96a3bb81a3f4e0c998bc7
Prem6567 <premkumarkanojiya758@gmail.com>
2026-07-08 18:32:04 +0530

    Update Add Java switch statement program
    Basic Java program demonstrating the switch statement with day names.

--- main.java
+++ main.java
@@ -1,12 +1,39 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 18;
+        int day = 7;
 
-        if (age >= 18) {
-            System.out.println("You are eligible to vote.");
-        } else {
-            System.out.println("You are not eligible to vote.");
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
         }
     }
 }
