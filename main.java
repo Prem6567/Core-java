d25189b60a44fe8faa30ff7c8d9b396e4a057982
Prem6567 <premkumarkanojiya758@gmail.com>
2026-07-09 18:17:46 +0530

    Update Add Java while loop program
    Added a basic Java while loop example that prints numbers from 1 to 5 using a while loop.

--- main.java
+++ main.java
@@ -1,39 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int day = 7;
+        int i = 2;
 
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-
-            case 2:
-                System.out.println("Tuesday");
-                break;
-
-            case 3:
-                System.out.println("Wednesday");
-                break;
-
-            case 4:
-                System.out.println("Thursday");
-                break;
-
-            case 5:
-                System.out.println("Friday");
-                break;
-
-            case 6:
-                System.out.println("Saturday");
-                break;
-
-            case 7:
-                System.out.println("Sunday");
-                break;
-
-            default:
-                System.out.println("Invalid Day");
+        while (i <= 10) {
+            System.out.println(i);
+            i += 2;
         }
     }
 }
