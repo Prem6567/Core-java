665484a7cd576fc7556b295fc377ee2e3000debd
Prem6567 <premkumarkanojiya758@gmail.com>
2026-07-13 13:33:03 +0530

    Update Java basics for loop program

--- main.java
+++ main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int i = 2;
-
-        while (i <= 10) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
-            i += 2;
         }
     }
 }
