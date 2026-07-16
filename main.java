8e61b2e17955762cfb3e79d4c300447e693dbe71
Prem6567 <premkumarkanojiya758@gmail.com>
2026-07-16 13:11:02 +0530

    Update  Java Basics Calculator project
    Added a simple console-based calculator using Java. The project uses Scanner for user input, switch-case for menu selection, if-else for division by zero handling, and arithmetic operators for addition, subtraction, multiplication, division, and modulus.

--- main.java
+++ main.java
@@ -1,8 +1,64 @@
-public class Main {
+import java.util.Scanner;
+
+public class Calculator {
+
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== JAVA BASIC CALCULATOR =====");
+
+        System.out.print(" Enter First Number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter Second Number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("\nChoose an Operation");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Modulus");
+        System.out.println("6. Exit");
+
+        System.out.print("Enter Your Choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+
+            case 1:
+                System.out.println("Result = " + (num1 + num2));
+                break;
+
+            case 2:
+                System.out.println("Result = " + (num1 - num2));
+                break;
+
+            case 3:
+                System.out.println("Result = " + (num1 * num2));
+                break;
+
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result = " + (num1 / num2));
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+
+            case 5:
+                System.out.println("Result = " + (num1 % num2));
+                break;
+
+            case 6:
+                System.out.println("Thank You!");
+                break;
+
+            default:
+                System.out.println("Invalid Choice!");
         }
+
+        sc.close();
     }
 }
