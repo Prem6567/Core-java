828605035b5d8d83b5508ec4c69041cc9f96969a
Prem6567 <premkumarkanojiya758@gmail.com>
2026-07-18 12:20:31 +0530

    Update  added Java  basics age calculator project
    Added a simple Java Age Calculator using Scanner input and basic arithmetic operations. The program calculates the user's age based on the birth year.

--- main.java
+++ main.java
@@ -1,62 +1,24 @@
 import java.util.Scanner;
 
-public class Calculator {
-
+public class AgeCalculator {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("===== JAVA BASIC CALCULATOR =====");
+        System.out.println("===== JAVA AGE CALCULATOR =====");
 
-        System.out.print(" Enter First Number: ");
-        double num1 = sc.nextDouble();
+        System.out.print("Enter Your Birth Year: ");
+        int birthYear = sc.nextInt();
 
-        System.out.print("Enter Second Number: ");
-        double num2 = sc.nextDouble();
+        System.out.print("Enter Current Year: ");
+        int currentYear = sc.nextInt();
 
-        System.out.println("\nChoose an Operation");
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
-        System.out.println("5. Modulus");
-        System.out.println("6. Exit");
+        int age = currentYear - birthYear;
 
-        System.out.print("Enter Your Choice: ");
-        int choice = sc.nextInt();
-
-        switch (choice) {
-
-            case 1:
-                System.out.println("Result = " + (num1 + num2));
-                break;
-
-            case 2:
-                System.out.println("Result = " + (num1 - num2));
-                break;
-
-            case 3:
-                System.out.println("Result = " + (num1 * num2));
-                break;
-
-            case 4:
-                if (num2 != 0) {
-                    System.out.println("Result = " + (num1 / num2));
-                } else {
-                    System.out.println("Cannot divide by zero.");
-                }
-                break;
-
-            case 5:
-                System.out.println("Result = " + (num1 % num2));
-                break;
-
-            case 6:
-                System.out.println("Thank You!");
-                break;
-
-            default:
-                System.out.println("Invalid Choice!");
+        if (age >= 0) {
+            System.out.println("Your Age is: " + age + " years");
+        } else {
+            System.out.println("Invalid Year Entered!");
         }
 
         sc.close();
